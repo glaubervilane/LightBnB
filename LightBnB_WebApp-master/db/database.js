74f7a4e518e0c0f4f813f0293e9601018b3aa220
Glauber Marques <glauberfmarques@gmail.com>
2023-07-12 19:06:23 -0600

    Implementing getAllReservations query to database

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -83,9 +83,13 @@ const addUser = function(user) {
  */
 const getAllReservations = function(guestId, limit = 10) {
   const queryString = `
-    SELECT *
+    SELECT reservations.*, properties.*, AVG(property_reviews.rating) AS average_rating
     FROM reservations
-    WHERE guest_id = $1
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1 AND reservations.end_date < now()::date
+    GROUP BY reservations.id, properties.id
+    ORDER BY reservations.start_date
     LIMIT $2;
   `;
   const values = [guestId, limit];
